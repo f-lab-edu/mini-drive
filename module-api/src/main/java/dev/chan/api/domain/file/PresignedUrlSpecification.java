04237e5386f45f5fabe5aabc49282dc9b12496d7
seongchanhwang12 <tjdcksghkd12@gmail.com>
2025-05-02 22:36:18 +0900

    refactor: 오타로 인한 필드명 수정

--- module-api/src/main/java/dev/chan/api/domain/file/PresignedUrlSpecification.java
+++ module-api/src/main/java/dev/chan/api/domain/file/PresignedUrlSpecification.java
@@ -12,18 +12,17 @@ public record PresignedUrlSpecification(
         String driveId,
         String parentId,
         String fileKey,
-        FileMetaData meta) {
+        FileMetaData metaData) {
     public static PresignedUrlSpecification toUrlSpec(String bucketName, PresignedUrlCommand command, String fileKey, FileMetaData meta) {
         return new PresignedUrlSpecification(bucketName, command.getDriveId(), command.getParentId(), fileKey, meta);
     }
-
     public Map<String,String> toS3Metadata(){
         return Map.of("driveId",driveId,
                 "parentId",parentId,
                 "fileKey",fileKey,
-                "size", String.valueOf(meta.getSize()),
-                "mimeType",meta.getMimeType(),
-                "name",meta.getName());
+                "size", String.valueOf(metaData.getSize()),
+                "mimeType",metaData.getMimeType(),
+                "name",metaData.getName());
     }
 }
 
